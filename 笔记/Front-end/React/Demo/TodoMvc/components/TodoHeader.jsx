5b1d7587cd7cce771a6612f24d67f7d43f707499
igcx <guo.chenxuya@gmail.com>
2022-01-28 00:16:28 +0800

    TodoMvc静态结构

---
+++ 笔记/Front-end/React/Demo/TodoMvc/components/TodoHeader.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class TodoHeader extends Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+          />
+        </header>
+      </div>
+    )
+  }
+}
